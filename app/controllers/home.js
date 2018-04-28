e5bc5880506e6eccdeaafe5773d3e2f856b2b667
andersonlimahw <andersonlimahw@gmail.com>
2018-04-28 14:33:13 -0300

    home

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -7,6 +7,10 @@ export default Ember.Controller.extend({
   actions: {
     continue(){
       this.toggleProperty('continue');
+    }, 
+    // #TODO, abrir modal de solicitação: passo 3 e 4
+    openModal(){
+      alert('continuar');
     }
   }
 });
