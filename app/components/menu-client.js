27fc129c5c1530d3611583459c0fedefccac62a5
andersonlimahw <andersonlimahw@gmail.com>
2018-04-29 15:38:18 -0300

    update menu

--- app/components/menu-client.js
+++ app/components/menu-client.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  scrollDuration: 750,
+  showNavbar: false, 
+
+  actions: {
+
+    scrollTo(section) {
+      // Ember.run.schedule("afterRender", () => {
+        if (section) {          
+          $("html,body").animate(
+            { scrollTop: $(section).offset().top - 50 }, 
+            this.get('scrollDuration'));
+        }
+      // });
+    }, 
+
+    toggleNavbar(navbar){
+      this.toggleProperty('showNavbar');
+      $(navbar).slideToggle();
+    }
+
+  }
 });
