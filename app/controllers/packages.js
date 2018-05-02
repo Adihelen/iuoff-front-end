f8c9d83292c0f1d5fa4e9dc08cee20938bafdafe
andersonlimahw <andersonlimahw@gmail.com>
2018-05-02 02:22:05 -0300

    update layout de pacteos mock with create record

---
+++ app/controllers/packages.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  // store: Ember.inject.service('store'),
+
+  // packageList: Ember.computed('model', function() {
+  //   return this.get('store').findAll('package')
+  // })
+});
--- app/routes/packages.js
+++ app/routes/packages.js
@@ -1,10 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {  
-  
+  model: function(){  
+    
     let packages = this.store.findAll('package');
+    console.log('packages from route: ', packages);
+    let newPackage =  this.store.createRecord('package', {
+      title: "Novo Pacote",
+      initialAmount: 1500.00, 
+      includeds: [
+        {
+          icon: "fa fa-home",
+          name: "4 Diárias"
+        }
+      ]
+    });
 
+    newPackage =  this.store.createRecord('package', {
+      title: "SP - Tokyo",
+      initialAmount: 2150.00, 
+      includeds: [
+        {
+          icon: "fa fa-smile",
+          name: "15 Diárias"
+        }
+      ]
+    });
     return packages;
   }
 });
