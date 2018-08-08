07e6781d824952821b60d1aaf6cf9288a731f45a
ylsapp <yulesapp@gmail.com>
2018-08-08 02:40:20 -0300

    Bug Correction

--- dist/assets/iuoff-client.js
+++ dist/assets/iuoff-client.js
@@ -2493,6 +2493,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("iuoff-client/app")["default"].create({"name":"iuoff-client","version":"0.0.0+5dd868e5"});
+  require("iuoff-client/app")["default"].create({"name":"iuoff-client","version":"0.0.0+4edd4dd3"});
 }
 //# sourceMappingURL=iuoff-client.map
